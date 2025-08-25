e46efb54c36c348ba4e07aca5fb8598c2502a42b
ej.omictin <earl.june.o@gmail.com>
2025-08-26 02:34:33 +0800

    main: TDD, for transaction model

--- src/test/java/com/simple/transaction/service/model/TransactionModelTest.java
+++ src/test/java/com/simple/transaction/service/model/TransactionModelTest.java
@@ -1,14 +1,13 @@
 package com.simple.transaction.service.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.Set;
 import java.util.stream.Stream;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -33,27 +32,27 @@ class TransactionModelTest {
 
   public static Stream<Arguments> invalidTransactionModels() {
     return Stream.of(
-        Arguments.of("No userId", "userId", TransactionModel.builder().build()),
-        Arguments.of("Negative userId", "userId", TransactionModel.builder().userId(-1L).build()),
-        Arguments.of("No Amount", "amount", TransactionModel.builder().build()),
+        Arguments.of("No userId", "Missing userId", TransactionModel.builder().build()),
+        Arguments.of(
+            "Negative userId",
+            "userId must be greater than 0",
+            TransactionModel.builder().userId(-1L).build()),
+        Arguments.of("No Amount", "Missing amount", TransactionModel.builder().build()),
         Arguments.of(
             "Negative Amount",
-            "amount",
+            "amount must be greater than 0",
             TransactionModel.builder().amount(BigDecimal.valueOf(-1)).build()),
-        Arguments.of("No type", "type", TransactionModel.builder().build()),
+        Arguments.of("No type", "Missing type", TransactionModel.builder().build()),
         Arguments.of(
-            "Incorrect type", "type", TransactionModel.builder().type("Incorrect").build()),
-        Arguments.of("No transaction date", "transactionDate", TransactionModel.builder().build()),
+            "Incorrect type",
+            "Type must be either DEBIT or CREDIT",
+            TransactionModel.builder().type("Incorrect").build()),
         Arguments.of(
-            "Past transaction date",
-            "transactionDate",
-            TransactionModel.builder()
-                .transactionDate(LocalDateTime.of(1990, Month.APRIL, 01, 01, 01))
-                .build()),
-        Arguments.of("No categoryId", "transactionDate", TransactionModel.builder().build()),
+            "No transaction date", "Missing transactionDate", TransactionModel.builder().build()),
+        Arguments.of("No categoryId", "Missing categoryId", TransactionModel.builder().build()),
         Arguments.of(
             "Negative categoryId",
-            "transactionDate",
+            "categoryId must be greater than 0",
             TransactionModel.builder().categoryId(-1L).build()));
   }
 }
