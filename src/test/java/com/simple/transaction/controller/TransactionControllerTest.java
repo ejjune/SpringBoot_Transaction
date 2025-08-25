7ee674340c5ea0f23d00c6e3c4c675984398a662
ej.omictin <earl.june.o@gmail.com>
2025-08-26 02:40:56 +0800

    main: tdd for transactioncontroller

--- src/test/java/com/simple/transaction/controller/TransactionControllerTest.java
+++ src/test/java/com/simple/transaction/controller/TransactionControllerTest.java
@@ -1,24 +1,33 @@
 package com.simple.transaction.controller;
 
-
 import com.simple.transaction.service.TransactionService;
+import com.simple.transaction.service.entity.Transaction;
+import com.simple.transaction.service.model.TransactionModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
 
 class TransactionControllerTest {
 
-    TransactionController transactionController;
+  TransactionController transactionController;
 
-    @BeforeEach
-    void init() {
-        TransactionService transactionService = Mockito.mock(TransactionService.class);
-        transactionController = new TransactionController(transactionService);
-    }
+  @BeforeEach
+  void init() {
+    TransactionService transactionService = Mockito.mock(TransactionService.class);
 
-    @Test
-    void shouldReturnTransactionWhenInputIsValid() {
+    Mockito.when(transactionService.addTransaction(Mockito.any()))
+        .thenReturn(Transaction.builder().build());
 
-    }
+    transactionController = new TransactionController(transactionService);
+  }
 
+  @Test
+  void shouldReturnTransactionWhenInputIsValid() {
+    ResponseEntity<Transaction> transactionResponseEntity =
+        transactionController.addTransaction(TransactionModel.builder().build());
+
+    Assertions.assertNotNull(transactionResponseEntity.getBody());
+  }
 }
